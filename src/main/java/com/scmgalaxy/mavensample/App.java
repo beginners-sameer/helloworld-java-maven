cf67108315c05c1417b07292398b108a51d5380e
beginners-sameer <49206176+beginners-sameer@users.noreply.github.com>
2020-10-11 00:17:38 +0530

    ER-2 Fixed for Welcome Page
    ER-2 Fixed for Welcome Page

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World RefX! BEG-2 --d" );
+        System.out.println( "Hello World modifcation" );
     }
 }
