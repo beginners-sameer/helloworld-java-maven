e0996d28577f186c5ee507e7331eef3694ffd742
Sameer Zulfi <devopssameer@outlook.com>
2020-10-17 18:30:47 +0530

    updated the folder structure

--- src/main/java/org/sonarqube/App.java
+++ src/main/java/org/sonarqube/App.java
@@ -1,4 +1,4 @@
-package com.scmgalaxy.mavensample;
+package org.sonarqube;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World modifcation" );
+        System.out.println( "Hello World!" );
     }
 }
